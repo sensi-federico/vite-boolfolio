43e0e54e186596f6d2dc5be52823a3ef404b78b8
sensiz <dev.federicosensi@gmail.com>
2023-01-19 18:08:13 +0100

    Milestone 5 complete

--- boolfolio/src/components/AppMain.vue
+++ boolfolio/src/components/AppMain.vue
@@ -1,49 +1,37 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
 export default {
     name: "AppMain",
     data() {
         return {
             projects: null,
-            base_api_url: 'http://localhost:8000',
+            base_api_url: "http://localhost:8000",
             loading: true,
-            error: null,
-            max: 100
-        }
+            error: null
+        };
     },
     methods: {
         getProjects(url) {
             axios
                 .get(url)
                 .then(response => {
-                    console.log(response.data.results)
-                    this.projects = response.data.results
-                    this.loading = false
+                    console.log(response.data.results);
+                    this.projects = response.data.results;
+                    this.loading = false;
                 })
                 .catch(error => {
-                    console.error(error)
-                    this.error = error.message
-                    this.loading = false
-                })
+                    console.error(error);
+                    this.error = error.message;
+                    this.loading = false;
+                });
         },
-        getImagePath(path) {
-            console.log(path)
-            if (path) {
-                return this.base_api_url + '/storage/' + path
-            }
-        },
-        trimDescription(text) {
-            if (text.lenght > this.max) {
-                return text.slice(0, this.max) + '...'
-            }
-            return text
-        }
-
     },
     mounted() {
-        this.getProjects(this.base_api_url + '/api/projects')
-    }
+        this.getProjects(this.base_api_url + "/api/projects");
+    },
+    components: { ProjectCard }
 }
 
 
@@ -52,9 +40,20 @@ export default {
 
 </script>
 
+
 <template>
-    <h1 class="text-center">Main</h1>
+    <section>
+        <div class="container">
+            <template v-if="projects">
+                <div class="row row-cols-4 g-4">
+                    <ProjectCard v-for="project in projects.data" :cover="project.cover" :title="project.title"
+                        :description="project.description" :type="project.type" :technologies="project.technologies" />
+                </div>
+            </template>
+        </div>
+    </section>
 </template>
+
 
 <style lang="scss" scoped>
 
---
+++ boolfolio/src/components/ProjectCard.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        title: String,
+        cover: String,
+        description: String,
+        type: String,
+        technologies: Array,
+    },
+    methods: {
+        getImagePath(path) {
+            console.log(path);
+            if (path) {
+                return this.base_api_url + "/storage/" + path;
+            }
+        },
+    }
+}
+</script>
+
+<template>
+
+    <div class="card border-0 shadow-sm rounded-0 rounded-bottom">
+        <img class="card-image rounded-top" :src="getImagePath(cover)" alt="">
+        <div class="card-body">
+            <h4>{{ title }}</h4>
+            <p>
+                {{ description }}
+            </p>
+            <a href="#">Read more</a>
+        </div>
+        <div class="card-footer text-muted">
+            <div class="type">
+                <strong>type: </strong>
+                <span v-if="type">
+                    {{ type.name }}
+                </span>
+                <span v-else>Uncategorized</span>
+            </div>
+            <div class="technologies">
+                <strong>technologies: </strong>
+
+                <span v-for="technology in technologies">
+                    #{{ technology.name }}
+                </span>
+            </div>
+        </div>
+    </div>
+
+</template>
